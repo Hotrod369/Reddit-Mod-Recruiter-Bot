7ac2e1f2d797867d750a37cde8d61a8e15a9357e
Hotrod <72291352+Hotrod369@users.noreply.github.com>
2023-04-01 07:56:25 -0400

    Update moderator_suggestor_reddit.py

--- moderator_suggestor_reddit.py
+++ moderator_suggestor_reddit.py
@@ -27,7 +27,13 @@ logging.basicConfig(
 )
 
 # Create a Reddit instance using PRAW
-reddit = praw.Reddit("bot1")
+reddit = praw.Reddit(
+    client_id="your_client_id",
+    client_secret="your_client_secret",
+    username="your_username",
+    password="your_password",
+    user_agent="your_user_agent",
+)
 
 
 # Input subreddit name
